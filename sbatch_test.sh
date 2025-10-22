a1742b0f64501929b0e3b03e77478ba8a0eab309
Daniele Merighi <merighi23@gmail.com>
2025-10-22 16:48:03 +0200

    Allow input file to be specified as an argument in sbatch_test.sh

--- sbatch_test.sh
+++ sbatch_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Input file (your .jsonl dataset)
-INPUT_FILE="mmtu.jsonl"
+INPUT_FILE="${1:-mmtu.jsonl}"  # Usa primo argomento o default mmtu.jsonl
 
 # Submit job to SLURM
 sbatch -N 1 --gpus=nvidia_geforce_rtx_3090:1 \
